7b2f7d4cc9387a4860551c7232eed5b8e893c8b2
keili <keili@LAPTOP-21J273G7>
2022-02-07 23:15:11 -0600

    Video 12 Y 13

--- FarmaStore/FarmaStore.BL/ProductosBL.cs
+++ FarmaStore/FarmaStore.BL/ProductosBL.cs
@@ -8,31 +8,17 @@ namespace FarmaStore.BL
 {
     public class ProductosBL
     {
+        Contexto _contexto;
+
+        public ProductosBL()
+        {
+            _contexto = new Contexto();
+        }
          public List<Producto> ObtenerProductos()
         {
-            var producto1 = new Producto();
-            producto1.Id = 1;
-            producto1.Descripcion = "Neurobion Dc 2500 con 1 jeringa";
-            producto1.Precio = 310;
+          
 
-            var producto2 = new Producto();
-            producto2.Id = 2;
-            producto2.Descripcion = "Oxivasc 10 Mg x 30 tabletas";
-            producto2.Precio = 292;
-
-            var producto3 = new Producto();
-            producto3.Id = 3;
-            producto3.Descripcion = "Pregavan 75 Mg x 30 Capsulas";
-            producto3.Precio =741;
-
-            var listadeProductos = new List<Producto>();
-            listadeProductos.Add(producto1);
-            listadeProductos.Add(producto2);
-            listadeProductos.Add(producto3);
-
-
-
-            return listadeProductos;
+            return _contexto.Prooductos.ToList(); ;
         }
 
         
