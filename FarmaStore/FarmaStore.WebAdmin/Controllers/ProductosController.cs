77f0f4b2807f91ebddf9e10d4e2fe7dcc3efd27e
AracelyMN <aracely18mejia@gmail.com>
2022-02-12 12:37:52 -0800

    video 18 Aracely M

---
+++ FarmaStore/FarmaStore.WebAdmin/Controllers/ProductosController.cs
@@ -0,0 +1,31 @@
+﻿using FarmaStore.BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FarmaStore.WebAdmin.Controllers
+{
+    public class ProductosController : Controller
+    {
+
+        ProductosBL _productosBL;
+
+        public ProductosController()
+        {
+            _productosBL = new ProductosBL();
+        }
+        
+
+
+        // GET: Productos
+        public ActionResult Index()
+        {
+
+            var listadeproductos = _productosBL.ObtenerProductos();
+
+            return View(listadeproductos);
+        }
+    }
+}
